16787747f1b967a382f4741257737a8c6e7f6859
Noah Cote <noahcote10@gmail.com>
2022-12-06 18:21:05 -0500

    completed local storage code. saved tasks now populate to the page when refreshed. begain README file.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# work-schedule
+# Work Day Scheduler
+
+## Description
+This project was to create a daily workday planner to record simple tasks that can be saved and will populate to the page when reloaded. This application uses the Dayjs API to display an updated day and time, and color the schedule differently depending on the time. This also uses local storage to record tasks.
+
+## Page Preview
+Below is what the page should look like:
+![workday-scheduler]()
+
+## Access
+This site can be found [here]().
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,22 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-// Global variables
-const currentDay = $('#currentDay');
-const saveBtn = $('.saveBtn')
+  // Global variables
+  const currentDay = $('#currentDay');
+  const saveBtn = $('.saveBtn')
 
-
-const hourlySchedule = [
-  {
-    time: '09',
-    task: ''
-  },
-  {
-    time: '10',
-    task: ''
-  },
-  {
-    time: '11',
-    task: ''
-  },
-  {
-    time: '12',
-    task: ''
-  },
-  {
-    time: '13',
-    task: ''
-  },
-  {
-    time: '14',
-    task: ''
-  },
-  {
-    time: '15',
-    task: ''
-  },
-  {
-    time: '16',
-    task: ''
-  },
-  {
-    time: '17',
-    task: ''
-  },
-]
+// Designites each hour block in military time so
+// function can color the block accordingly
+  const hourlySchedule = [
+    '09',
+    '10',
+    '11',
+    '12',
+    '13',
+    '14',
+    '15',
+    '16',
+    '17',
+  ]
 
 
   // TODO: Add a listener for click events on the save button. This code should
@@ -64,61 +38,64 @@ const hourlySchedule = [
 
 
 
-// Displays day/time
-function displayTime() {
-  const rightNow = dayjs().format('dddd, MMMM DD, YYYY [at] h:mm:ss A');
-  currentDay.text(rightNow);
-}
-
-// checks the status of each time block and changes its color accordingly
-function statusCheck() {
-  for (let i = 0; i < hourlySchedule.length; i++) {
-    const currentTime = dayjs().format('HH');
-    const timeBlock = hourlySchedule[i].time
-    const classTag = $('.task-'+i);
-
-    if (timeBlock < currentTime) {
-      classTag.addClass('past');
-      classTag.removeClass('present');
-      classTag.removeClass('future');
-      console.log('past')
-    } else if (timeBlock > currentTime) {
-      classTag.removeClass('past');
-      classTag.removeClass('present');
-      classTag.addClass('future');
-      console.log('future')
-    } else {
-      classTag.removeClass('past');
-      classTag.addClass('present');
-      classTag.removeClass('future');
-      console.log('present')
-    }
-
+  // Displays day/time
+  function displayTime() {
+    const rightNow = dayjs().format('dddd, MMMM DD, YYYY [at] h:mm:ss A');
+    currentDay.text(rightNow);
   }
-}
-// Saves the tasks to local storage
-saveBtn.on('click', function (event){
-  event.preventDefault();
 
-  const taskHour = $(this).data("time");
-  const taskText = $("#" + taskHour).val();
+  // checks the status of each time block and changes its color accordingly
+  function statusCheck() {
+    for (let i = 0; i < hourlySchedule.length; i++) {
+      const currentTime = dayjs().format('HH');
+      const timeBlock = hourlySchedule[i]
+      const classTag = $('.task-' + i);
 
-  localStorage.setItem(taskHour, taskText);
+      if (timeBlock < currentTime) {
+        classTag.addClass('past');
+        classTag.removeClass('present');
+        classTag.removeClass('future');
+        console.log('past')
+      } else if (timeBlock > currentTime) {
+        classTag.removeClass('past');
+        classTag.removeClass('present');
+        classTag.addClass('future');
+        console.log('future')
+      } else {
+        classTag.removeClass('past');
+        classTag.addClass('present');
+        classTag.removeClass('future');
+        console.log('present')
+      }
 
-});
+    }
+  }
+  // Saves the tasks to local storage
+  saveBtn.on('click', function (event) {
+    event.preventDefault();
 
-//Renders any saved tasks to page on load/refresh
-// UNDER CONSTRUCTION
-// function renderSavedTasks() {
-//   for (let i = 0; i < array.length; i++) {
-//     const element = array[i];
-    
-//   }
-// }
+    const taskHour = $(this).data("time");
+    const taskText = $("#" + taskHour).val();
 
-// Updates the day/time every second
-displayTime();
-setInterval(displayTime, 1000);
+    localStorage.setItem(taskHour, taskText);
 
-statusCheck();
+  });
+
+  //Renders any saved tasks to page on load/refresh
+  function renderSavedTasks() {
+    for (let i = 9; i <= 17 ; i++) {
+      const task = localStorage.getItem(i);
+
+      if (task) {
+        $(`#${i}`).val(task);
+      }
+      
+    }
+  }
+
+  // Updates the day/time every second
+  displayTime();
+  setInterval(displayTime, 1000);
+  renderSavedTasks();
+  statusCheck();
 });
